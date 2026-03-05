92a08fb21b7d44d3d223765d52e635f996bf5ee7
Miro Home PC <miro@home.pc>
2026-03-05 21:06:02 +0100

    Moved to dedicated directory

---
+++ DU_1/du-1_universal.py
@@ -0,0 +1,10 @@
+import base64
+
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
+def decode_image(encoded_string, output_path):
+    with open(output_path, "wb") as output_file:
+        output_file.write(base64.b64decode(encoded_string)) 
+
